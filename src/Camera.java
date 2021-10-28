c4b819b287a7146236552f410455c2c7f1a6d21b
Nassim_Rami <nassim.rami99@gmail.com>
2021-10-28 11:54:15 +0100

    28/10

--- src/Camera.java
+++ src/Camera.java
@@ -1,20 +1,32 @@
 public class Camera {
     private double xOffset;
     private double yOffset;
+    private double vx ;
+
+
+    private double ax;
+
+    private Heros monHeros;
+
+    private final double k=1;
+    private final double f=1.2;
 
     public Camera(double x , double y )
     {
         this.xOffset=x;
         this.yOffset=y;
+    }
 
+
+    public void update(double time)
+    {
+        ax=(monHeros.getX()-xOffset)*k-(f*vx);
+        vx=vx+ax*time;
+        xOffset=xOffset+vx*time;
 
     }
 
-    public void move(float x , float y)
-    {xOffset+=x;
-     yOffset+=y;
 
-    }
 
     public double getcX()
     {
